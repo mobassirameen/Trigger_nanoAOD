87a3b1239fe20680baaf8b77281bea8f6aa3c6d3
Mohammad Mobassir Ameen <mohammad.mobassir.ameen@cern.ch>
2024-06-29 08:36:07 +0200

    2D eff plot script

--- plotEff.py
+++ plotEff.py
@@ -77,58 +77,8 @@ colors = {0: ROOT.kBlack,
 def main(args):
 
     f = ROOT.TFile(args.rfile, "READ")
-    #fdir = f.GetDirectory("metTrigAnalyzerNanoAOD")
     fdir = f.GetDirectory("twoleptonTriggers")
     statOption = ROOT.TEfficiency.kFCP
-
-    #variables  = ["met_pt", "pv"]
-    variables  = ["lep1_pt", "lep1_eta"]
-    #triggers   = ["passed", "passtrig_HLT_Ele32_WPTight_Gsf", "passtrig_HLT_IsoMu24", "passtrig_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL"]
-    #triggers   = ["passed", "passtrig_HLT_Ele32_WPTight_Gsf"]
-    triggers   = ["passedsignalANDreference"]
-
-    for var in variables:
-        c = getCanvas()
-        leg = createLegend()
-        #den = fdir.Get(f'h_{var}_all')
-        den = fdir.Get(f'h_{var}_passedreftrig')
-
-        nums = {}
-        effs = {}
-        for j, trg in enumerate(triggers):
-            nums[trg] = fdir.Get(f'h_{var}_{trg}')
-            effs[trg] = ROOT.TEfficiency(nums[trg], den)
-            effs[trg].SetStatisticOption(statOption)
-            effs[trg] = SetStyle(effs[trg], colors[j])
-            if j == 0:
-                effs[trg].Draw()
-            else:
-                effs[trg].Draw("same")
-            leg.AddEntry(effs[trg], trg.replace("passtrig_HLT", "HLT").replace("passedsignalANDreference", "logical OR"), "ep")
-                
-        c.Modified()
-        c.Update()
-        effs["passedsignalANDreference"].GetPaintedGraph().GetYaxis().SetRangeUser(0.0, 1.2)
-        effs["passedsignalANDreference"].GetPaintedGraph().GetYaxis().SetTitle("#varepsilon_{L1+HLT}")
-        leg.Draw("same")
-        
-        # Styling stuff
-        tex_cms = AddCMSText()
-        tex_cms.Draw("same")
-        
-        private = AddPrivateWorkText()
-        private.Draw("same")
-        
-        header = ROOT.TLatex()
-        header.SetTextSize(0.04)
-        header.DrawLatexNDC(0.57, 0.905, "2022EE, #sqrt{s} = 13.6 TeV")
-        
-        c.Update()
-        c.Modified()
-        for fs in args.formats:
-            savename = f'plots/TrgEffs_{var}{fs}'
-            c.SaveAs(savename)
-            
 
     # Plot 2D:
     c2D = getCanvas()
@@ -169,25 +119,27 @@ def main(args):
 
     # Save the plot in specified formats
     for fs in args.formats:
-        c2D.SaveAs("plots/Eff2D_trigger_EtavsPt%s" % (fs))     
-    
+        c2D.SaveAs("/eos/user/m/moameen/www/TriggerStudyPlots/Eff2D_trigger_lep1_eta_vs_pt%s" % (fs))
+        
+'''  
+    ########################################
     # Print the numerical efficiency values
-    #print("Efficiency values (eta, pt, efficiency):")
-    #for x_bin in range(1, eff2D.GetTotalHistogram().GetNbinsX() + 1):
-        #for y_bin in range(1, eff2D.GetTotalHistogram().GetNbinsY() + 1):
-            #if eff2D.GetTotalHistogram().GetBinContent(x_bin, y_bin) != 0:  # Check to avoid division by zero
-                #eta = eff2D.GetTotalHistogram().GetXaxis().GetBinCenter(x_bin)
-                #pt = eff2D.GetTotalHistogram().GetYaxis().GetBinCenter(y_bin)
-                #efficiency = eff2D.GetEfficiency(eff2D.GetGlobalBin(x_bin, y_bin))
-                #print(f"eta: {eta}, pt: {pt}, efficiency: {efficiency}")
+    ########################################
+    print("Efficiency values (eta, pt, efficiency):")
+    for x_bin in range(1, eff2D.GetTotalHistogram().GetNbinsX() + 1):
+        for y_bin in range(1, eff2D.GetTotalHistogram().GetNbinsY() + 1):
+            if eff2D.GetTotalHistogram().GetBinContent(x_bin, y_bin) != 0:  # Check to avoid division by zero
+                eta = eff2D.GetTotalHistogram().GetXaxis().GetBinCenter(x_bin)
+                pt = eff2D.GetTotalHistogram().GetYaxis().GetBinCenter(y_bin)
+                efficiency = eff2D.GetEfficiency(eff2D.GetGlobalBin(x_bin, y_bin))
+                print(f"eta: {eta}, pt: {pt}, efficiency: {efficiency}")
+'''
 
 if __name__ == "__main__":
 
     VERBOSE       = True
     YEAR          = "2022EE"
-    #TRGROOTFILE   = "histos_2lssTrigger_DATA2022EE.root"
-    #TRGROOTFILE   = "histos_2lssTrigger_pt_eta_DATA2022EE.root"
-    TRGROOTFILE   = "histos_2lssTrigger_pt_eta_1D_2D_DATA2022EE.root"
+    TRGROOTFILE   = "histos_2lssTrigger_DATA2022EE.root"
     FORMATS       = ['.png', '.pdf']
 
     parser = ArgumentParser(description="Derive the trigger scale factors")
